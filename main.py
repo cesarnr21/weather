3cb0fd348743b3091374ff4030d3f4e6e98ec1ae
Cesar Nunez Rodriguez <58354773+cesarnr21@users.noreply.github.com>
2021-11-30 02:36:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+
+import csv
+import matplotlib as plt
+import plot
+import difference
+import report
+
+"""
+outline:
+
+report feature: allows you to print an image with weather and some symbols
+- temperature
+- rain, fog, snow, cloudy
+
+plot feature: plots the temperature for either the year or a single month
+
+maybe another one
+"""
+
+class taskSelector:
+    pass
+    # timePeriod
+    # task
+
+
+
+print("Available Functions:")
+print("Report \nPlot \nOther")
+
+taskSelector = input()
+
+def call(task):
+    case(report):
+        report(timePeriod)
+
+    case(plot):
+        plot(timePeriod)

5df201bc151bc8f6b467554353ff33cc84f180f8
cesarnr21 <cesarnr21@gmail.com>
2022-02-20 12:58:11 -0500

    add packages

--- get_weather.py
+++ get_weather.py
@@ -3,7 +3,11 @@
 # find the location, maybe using IP address, or find a way to get location
 # figure out a way to add options when calling the python scrip
 
+from ipaddress import ip_address
+from re import I
 import sys
+import configparser
+import urllib
 
 __version__ = '1.0'
 
@@ -13,23 +17,27 @@ def create_target():
     if len(args) == 0:
         get_ip_location()
     else:
-        def command_select(args):
+        def command_select(input):
             commands = {
                 help : "--help",
                 config : "--config",
             }
 
-            return commands.get(args, "Unknown Command, available commands: --help and --config")
+            return commands.get(input, "Unknown Command, available commands: --help and --config")
+        
+        command_select(args)
 
-        pass # 
+        pass
     pass
 
 def get_ip_location(target):
     """
     In case no location is selected, find the IP address location and use that instead
     """
+    url = "http://checkip.dyndns.org"
+    ip_address = urllib.urlopen(url).read()
+    target = ip_address[-30:-16]
     print(target)
-    pass
 
 
 def get_data(location):
@@ -38,6 +46,9 @@ def get_data(location):
     """
     print(location)
     pass
+
+def print_data():
+    print("Weather in <insert location> \nTemperature: \n Temperature Feels Like: ")
 
 def main():
     target_location = create_target()

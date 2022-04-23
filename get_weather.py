0bb201e8d312c9ca39d29957a0af7d3271118291
cesarnr21 <cesarnr21@gmail.com>
2022-04-23 16:11:39 -0400

    fix issues

--- get_weather.py
+++ get_weather.py
@@ -6,13 +6,11 @@ import urllib.request
 __version__ = '1.0'
 
 def get_ip_location():
-    """
-    Get the users location using the IP address
-    """
     ip_address = urllib.request.urlopen("http://checkip.dyndns.org").read() 
     ip_address = ip_address[-30:-16]
     ip_address = ip_address.decode("utf-8")
     print("IP Address: " + ip_address)
+    ip_address = ip_address[1:]
 
     location_data = urllib.request.urlopen("http://ip-api.com/json/" + ip_address).read()
     location_data = location_data.decode("utf-8")
@@ -20,43 +18,42 @@ def get_ip_location():
     return location
 
 def get_data(location, info):
-    """
-    Uses API to find weather data for the selected location
-    """
     api = 'https://api.openweathermap.org/data/2.5/weather?'
     api_link = api + 'lat=' + str(location['lat']) + '&lon=' + str(location['lon']) + '&units=' + \
         info['report_unit'] + '&appid=' + info['api_key']
+
     weather_data = urllib.request.urlopen(api_link).read()
     weather_data = weather_data.decode("utf-8")
     weather = json.loads(weather_data)
     return weather
 
-# add if config file is not created then call on save_config
-def load_config():
-    """
-    Loads the data from the configuration file
-    """
-    with open('actual.json', 'r') as file:
-    #with open('config.json', 'r') as file:
-        config = json.load(file)
-    return config
-
 def save_config():
-    """
-    Allows the user to create and save the file
-    """
     print("If you do not have an API key for OpenWeather then go to https://openweathermap.org/ \
-        and sign up for a free API Key, then enter it below.")
+    and sign up for one. Then enter you API key below.")
+
     api_key = input("Enter your API Key: ")
     print("The weather can reported in Farenheit, Celsius, or Kelvin\
-        \nFor Kelvin enter 'standard', Celsius: 'metric', Fahrenheit: 'imperial'")
+    \nFor Kelvin enter 'standard', Celsius: 'metric', Fahrenheit: 'imperial'")
+
     unit = input("Enter desired weather unit: ")
-    #unit = 'imperial' # can also be either standard or metric
     config = {'api_key' : api_key, 'report_unit' : unit}
+
     # this still needs more work
-    with open('config.json', 'w') as file:
+    with open(settings, 'w+') as file:
         output = json.dumps(config, indent = 4)
         file.write(output)
+
+def load_config():
+    try:
+        with open(settings, 'r') as file:
+            config = json.load(file)
+
+        return config
+
+    except FileNotFoundError:
+        print("Creating configuration file to store API key and settings\n")
+        save_config()
+        load_config()
 
 # need find a better wait to format the output
 def display_data(weather):
@@ -75,8 +72,8 @@ def select_task():
 
     elif args[0] == "--status":
         info = load_config()
-        print("API Key in file is: " + info['api_key'])
-        print("Weather Data will be reported using " + info['report_unit'] + " units")
+        print("API Key in file is: ", info['api_key'])
+        print("Weather Data will be reported using ", info['report_unit'], "units")
         exit(0)
 
     elif args[0] == "--config":
@@ -84,10 +81,13 @@ def select_task():
         exit(0)
 
     else:
-        print(args[0], "command not avialable. Try again.")
+        print(args[0], "The argument was not recognized. Try again.")
         exit(0)
 
 def main():
+    global settings
+    settings = "try.json"
+    #settings = "config.json"
     select_task()
     weather_data = get_data(get_ip_location(), load_config())
     display_data(weather_data)
